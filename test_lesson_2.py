9c1c2c91ca03f0cdd12d7408060c4efecf17ab73
Elizaveta <eecherkasova@mail.ru>
2023-11-09 21:06:32 +0300

    добавлен файл requirements.txt

--- test_lesson_2.py
+++ test_lesson_2.py
@@ -1,22 +1,23 @@
 import pytest
+import selene
 from selene import be, have
-from selene.support.shared import browser
+
 
 @pytest.fixture
 def window():
-    browser.config.window_height(300)
-    browser.config.window_width(500)
+    selene.browser.config.window_height = 300
+    selene.browser.config.window_width = 500
 
 
 
-def test_google_search():
-    browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
-    browser.quit()
+def test_google_search(window):
+    selene.browser.open('https://google.com')
+    selene.browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    selene.browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+    selene.browser.quit()
 
-def test_no_result():
-    browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('qaswdefrgtyhujk').press_enter()
-    browser.element('[class="card-section"]').should(have.text('По запросу qaswdefrgtyhujk ничего не найдено'))
+def test_no_result(window):
+    selene.browser.open('https://google.com')
+    selene.browser.element('[name="q"]').should(be.blank).type('qaswdefrgtyhujk').press_enter()
+    selene.browser.element('[class="card-section"]').should(have.text('По запросу qaswdefrgtyhujk ничего не найдено'))
 
